2ff6281877afaa71d3310ef8d5921f830b7bafc8
aseel <aseelissa91@hotmail.com>
2020-12-17 11:44:34 +0200

    adding matrix exercises

--- src/EmployeeMatrix.js
+++ src/EmployeeMatrix.js
@@ -1,10 +1,45 @@
 /* Write your code below */
+const Matrix = require('./Matrix.js')
+class EmployeeMatrix extends Matrix{
 
+    constructor(){
+        super(0,0)
+    }
+
+    loadData(salaryData){
+       // this.generateMatrix(salaryData.length, 1)
+        for(let i=0; i<salaryData.length; i++){
+           // console.log(this.Matrix)
+            this.matrix.push(Object.values(salaryData[i]))
+        }
+    }
+
+}
 
 
 
 //You can paste the code from the lesson below to test your solution
+let data = [
+    { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
+    { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
+    { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
+    { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
+    { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
+    { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 }
+]
 
+let m = new EmployeeMatrix()
+
+m.loadData(data)
+m.print()
+//prints
+
+// e10021  Gillian Finance 2000
+// e10725  Tibor   Design  1200
+// e10041  Anisha  Finance 2300
+// e10411  Jakub   Design  1600
+// e11995  Mar     Design  1300
+// e10732  Nisha   Design  1200
 
 
 
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,12 +1,82 @@
 /* Write your code below */
+class Matrix{
+    constructor(rows, cols){
+        this.matrix = []
+        this.generateMatrix(rows, cols)
+    }
+    generateMatrix(rows, cols){
+        let row
+        let counter = 1
+        for(let i = 0; i<rows; i++){
+            row = []
+            for(let j=0; j<cols; j++){
+                row.push(counter++)
+            }
+            this.matrix.push(row)
+        }
+    }
 
+    print(){
+        for(let i=0; i<this.matrix.length; i++){
+            console.log(this.matrix[i])
+        }
+    }
+
+    alter(row, col, value){
+        this.matrix[row][col] = value
+    }
+
+    get(row, col){
+        return this.matrix[row][col]
+    }
+
+    printColumn(col){
+        for(let i=0; i<this.matrix.length; i++){
+            console.log(this.matrix[i][col])
+        }
+    }
+
+    printRow(row){
+        for(let i=0; i<this.matrix[row].length; i++){
+            console.log(this.matrix[row][i])
+        }
+    }
+
+    findCoordinate(value){
+        for(let i = 0; i<this.matrix.length; i++){
+            for(let j=0; j<this.matrix[i].length; j++){
+                if(value == this.matrix[i][j]){
+                    return {x: i, y:j}
+                }
+            }
+        }
+        return {err: "This number does not exist in this matrix"}
+    }
+
+}
 
 
 
 //You can paste the code from the lesson below to test your solution
 
+let m = new Matrix(3, 4)
+m.print()
+//prints
+/*
+1       2       3       4
+5       6       7       8
+9       10      11      12
+*/
+
+m.alter(0, 0, m.get(1, 1))
+m.printColumn(0) //prints 6, 5, 9 (separate lines)
+m.printRow(0) //prints 6, 2, 3, 4 (separate lines)
 
 
+m = new Matrix(3, 4)
+m.print()
+console.log(m.findCoordinate(12)) //prints {x: 3, y: 2}
+console.log(m.findCoordinate(7)) //prints {x: 2, y: 1}
 
 /* Do not remove the exports below */
 module.exports = Matrix
